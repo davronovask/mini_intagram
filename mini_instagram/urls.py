0e57a1d5865d585fd2e68a0acc863a676644b274
davronovask <abdulkhaevamuslima07@gmail.com>
2024-11-27 19:53:10 +0600

    че то делала

--- mini_instagram/urls.py
+++ mini_instagram/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.static import static
 from django.urls import path
 
 from users.views import UserRegistrationView, LoginPageView, UserMakeLoginView, UserMakeRegistrationView
+from posts.views import ProfileView, ReelsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,9 @@ urlpatterns = [
     path('login/', LoginPageView.as_view(), name='login-url'),  # Добавляем имя для пути логина
     path('make-login/', UserMakeLoginView.as_view(), name='make-login-url'),
     path('make-registration/', UserMakeRegistrationView.as_view(), name='make-registration-url'),
+
+    path('profile/', ProfileView.as_view(), name='profile-url'),
+    path('reels/', ReelsView.as_view(), name='reels-url'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+class ProfileView(TemplateView):
+    template_name = 'profile.html'
+
+
+
+
+class ReelsView(TemplateView):
+    template_name = 'reels.html'
+
